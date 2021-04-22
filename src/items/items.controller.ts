4af0aa7f5ec100a8aa7b3c88a471834095b1c9f5
Golam Saroar <gsaroar@testreach.com>
2021-04-23 02:06:29 +0600

    basic crud methods created

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,5 +1,5 @@
 import { CreateItemDto } from './dto/create.item.dto';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('items')
 export class ItemsController {
@@ -8,8 +8,25 @@ export class ItemsController {
     findAll() :string {
         return 'Get all items';
     }
+    @Get(':id')
+    findOne(@Param() param) :string {
+        return `the item is ${param.id}`;
+    }
     @Post()
     create(@Body() createItemDto: CreateItemDto): string {
         return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
     }
+    
+    @Put(':id')
+    update(@Param() param, @Body() updateItemDto: CreateItemDto): string {
+        return `update for ${param.id} Name: ${updateItemDto.name}`
+
+    }
+    @Delete(':id')
+    delete(@Param() param) {
+        return `deleted ${param.id}`;
+    }
+
+
+
 }
